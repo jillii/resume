e3c7045ec78c08da91e71abfbf735c1c23584353
jillii <jho3292@gmail.com>
2024-10-06 16:30:24 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const HEADER = {
     name: "Jill Hoenig",
-    position: "Lead Fullstack Engineer",
-    about: "Hey I'm Jill ~ a senior fullstack engineer with 8+ years of experience and a strong foundation. Ask me about my side projects."
+    position: "Senior Engineer",
+    about: "Hey I'm Jill ~ a senior engineer specializing in frontend development with 8+ years of experience and a strong foundation. Ask me about my side projects."
 }
 export const RESUME = [
     {
@@ -28,20 +28,20 @@ export const RESUME = [
                 break: true,
                 isList: true,
                 sectionHead: "Ideometry",
-                title: "Lead Developer",
+                title: "Senior Engineer",
                 subtitle: "DECEMBER 2021 - PRESENT, BOSTON MA",
                 copy: 'Spearheaded web projects from conception to delivery, adhering to stringent coding standards and elegant, efficient code. Pioneered implementation of continuous integration, enabling seamless collaboration within the Ideometry dev team. Cultivated fun work environment characterized by a relaxed yet productive culture, fostering a team ethos centered on delivering high quality output. Crafted and executed comprehensive professional development initiative tailored for junior developers, equipping them with essential skills like Git, SASS, and JavaScript to enhance their proficiency and contribution to the team\'s success.'
             },
             {
                 isList: true,
-                title: "Fullstack Developer",
+                title: "Frontend Engineer",
                 subtitle: "MAY 2017 - DECEMBER 2021, BOSTON MA",
                 copy: 'Carried out entire lifecycle of web projects, from server creation to QA testing, ensuring client satisfaction. Collaborated with design team to execute innovative, award-winning websites. Prioritized development of efficient user-friendly sites, emphasizing scalability to meet evolving client needs and industry standards.'
             },
             {
                 isList: true,
                 sectionHead: "Isovera",
-                title: "Web Developer",
+                title: "Staff Engineer",
                 subtitle: "NOVEMBER 2015 - MAY 2017, WALTHAM MA",
                 copy: "Elevated user experience of Isovera's internal website through strategic enhancements and user-centered design principles. Provided crucial development support and technical expertise to bolster development team."
             }
