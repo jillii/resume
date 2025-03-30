87ff41c9366b62a5bad731ec70dade434404388d
Jill <jho3292@gmail.com>
2025-03-30 12:03:16 -0400

    Nina edits.

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const HEADER = {
     name: "Jill Hoenig",
     position: "Senior Engineer",
-    about: "Seasoned Web Engineer with 10 years of industry experience spearheading web projects from inception to completion, with a focus on scalability and innovative solutions to complex challenges. Two years of experience leading dev team, dedicated to fostering a culture of continuous learning and instilling pride in our work."
+    about: "Senior Web Engineer with 10 years experience leading web projects from inception to completion, focusing on scalability and innovative solutions to complex challenges. Two years experience leading dev teams. Committed to fostering a culture of continuous learning and instilling pride in our work."
 }
 export const RESUME = [
     {
@@ -29,21 +29,21 @@ export const RESUME = [
                 isList: true,
                 sectionHead: "Ideometry",
                 title: "Senior Engineer",
-                subtitle: "DECEMBER 2021 - MARCH 2025, BOSTON MA",
-                copy: 'Led web project development, ensured adherence to strict coding standards and efficient code practices. Implemented continuous integration processes to facilitate seamless team collaboration. Promoted a productive and enjoyable work environment, with a culture focused on high-quality deliverables. Developed and implemented a professional development program for junior developers, focusing on key skills such as Git, command-line usage, and JavaScript.'
+                subtitle: "DECEMBER 2021 - MARCH 2025, REMOTE",
+                copy: 'Led web project development, ensuring adherence to coding standards and efficient practices. Implemented continuous integration processes to streamline team collaboration. Fostered a productive and enjoyable work environment, emphasizing high-quality deliverables. Developed and implemented a professional development program for junior developers, focusing on key skills such as Git, command-line usage, and JavaScript.'
             },
             {
                 isList: true,
                 title: "Frontend Engineer",
-                subtitle: "MAY 2017 - DECEMBER 2021, BOSTON MA",
-                copy: 'Executed full lifecycle of web development projects, including server setup and QA testing, to meet client expectations. Partnered with the design team to deliver innovative websites that received industry recognition. Focused on developing efficient, user-friendly websites with scalability to adapt to changing client requirements and industry trends.',
+                subtitle: "MAY 2017 - DECEMBER 2021, BOSTON MA - REMOTE",
+                copy: 'Executed the full lifecycle of website projects, including server setup and QA testing, to meet client expectations. Collaborated with design team to deliver innovative websites that garnered industry recognition, including the CUNA Diamond Award winning Quorum Learning Hub. Focused on creating efficient, user-friendly websites with scalability to adapt to evolving client needs and industry trends.',
             },
             {
                 isList: true,
                 sectionHead: "Isovera",
-                title: "Staff Engineer",
+                title: "Frontend Engineer",
                 subtitle: "NOVEMBER 2015 - MAY 2017, WALTHAM MA",
-                copy: "Elevated user experience of Isovera's internal website through strategic enhancements and user-centered design principles. Provided crucial development support and technical expertise to bolster development team."
+                copy: "Improved user experience of company's internal web platform through strategic improvements and user-centered design principles, such as adding icons and ensuring ADA compliance. Provided key development support and technical expertise to strengthen the dev team."
             }
         ]
     },
@@ -51,7 +51,7 @@ export const RESUME = [
         title: "Contact",
         content: [
             {
-                contact: ['201-562-5969', 'jho3292@gmail.com']
+                contact: ['(201) 562-5969', 'hello@jill.digital']
             },
             {
                 suptext: "REPOS",
@@ -75,26 +75,26 @@ export const RESUME = [
             },
             {
                 sectionHead: "jillii.github.io/resume",
-                title: "React app 'resume'",
-                copy: 'Yes, this resume is a React app, (although you might be viewing the PDF version). Reactified resume for organization and easy updates.',
+                title: "React app resume",
+                copy: 'Yes, this resume is a React app (although you might be viewing the PDF version). Reactified resume for organization and easy updates.',
                 link: "https://jillii.github.io/resume/"
             },
             {
                 sectionHead: "expletive.plumbing",
                 title: "Portfolio site for IDM project !@#$%",
-                copy: 'Original design with multiple "skins". Fully functional media player built with plain JavaScript, featuring shuffle play, draggable playback slider, and filter play functionality. Built with flat database structure to ensure lightning fast page load.',
+                copy: 'Original design with multiple skins. Fully functional media player built with vanilla JavaScript, featuring shuffle play, draggable playback slider, and filter play functionality. Built with flat database structure to ensure lightning-fast page load.',
                 link: "https://expletive.plumbing"
             },
             {
                 sectionHead: "cakefactory.party",
                 title: "Official site for rave-organizing collective",
-                copy: "Comprehensive platform housing information about releases and upcoming shows. I added video streaming capabilities and live chat functionality to adapt to the shift towards online performances during the COVID.",
+                copy: "Comprehensive platform featuring information on releases and upcoming shows. I added video streaming and live chat functionality to host online performances during COVID.",
                 link: "https://cakefactory.party"
             },
             {
                 sectionHead: "designstatements-llc.com",
                 title: "Home for Design Statements LLC",
-                copy: "Bespoke WordPress site featuring custom theme inspired by paint chips utilized by designers.",
+                copy: "Bespoke WordPress site for an interior designer client featuring custom theme inspired by paint chips.",
                 link: "http://designstatements-llc.com"
             },
             {
